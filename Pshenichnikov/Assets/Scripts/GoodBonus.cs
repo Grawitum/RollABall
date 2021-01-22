988c4fd2c3367f2a94bb671a176716d7cf5d93b4
Grawitum <mackc1m@yandex.ru>
2021-01-22 14:40:41 +0500

    Creat Bonus
    CreatGood Bonus

---
+++ Pshenichnikov/Assets/Scripts/GoodBonus.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RollABall
+{
+    public class GoodBonus : InteractiveObject
+    {
+        protected override void Interaction()
+        {
+            base.Interaction();
+            print("Check");
+        }
+    }
+}
