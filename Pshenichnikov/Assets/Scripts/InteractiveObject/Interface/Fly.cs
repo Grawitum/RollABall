f380e37527bf05ddc68f2f1f62aec6ed8c2254c6
Grawitum <mackc1m@yandex.ru>
2021-01-23 14:46:57 +0500

    Write Cod
    Write Cod goodbonus, bedbonus

---
+++ Pshenichnikov/Assets/Scripts/InteractiveObject/Interface/Fly.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RollABall
+{
+    public interface IFly
+    {
+        void Fly();
+        //{
+            //Debug.Log("I can fly");
+        //}
+    }
+}
---
+++ Pshenichnikov/Assets/Scripts/InteractiveObject/SpeedBonus.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RollABall
+{
+    public sealed class SpeedBonus : InteractiveObject, IFly, IFlicker
+    {
+        private Material _material;
+        private float _lengthFly;
+        private PlayerBall playerBall;
+
+        private void Awake()
+        {
+            _material = GetComponent<Renderer>().material;
+            _lengthFly = Random.Range(1.0f, 3.0f);
+            playerBall = FindObjectOfType<PlayerBall>();
+        }
+
+        protected override void Interaction()
+        {
+            //base.Interaction();
+            playerBall.SpeedUP();
+            
+        }
+        public void Fly()
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x,
+                Mathf.PingPong(Time.time, _lengthFly),
+                transform.localPosition.z);
+        }
+
+        public void Flicker()
+        {
+            _material.color = new Color(_material.color.r, _material.color.g, _material.color.b,
+                Mathf.PingPong(Time.time, 1.0f));
+        }
+    }
+}
