d0e5de9085e4cf5c570300427859fef16bf2306b
Grawitum <mackc1m@yandex.ru>
2021-01-23 15:20:02 +0500

    Write Cod v2.0
    Write cod and HW task

---
+++ Pshenichnikov/Assets/Scripts/InteractiveObject/GoodBonus.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RollABall
+{
+    public sealed class GoodBonus : InteractiveObject, IFly, IFlicker
+    {
+        public int Point;
+        private Material _material;
+        private float _lengthFly;
+        private DisplayBonuses _displayBonuses;
+        private GoodBonus[] _goodBonus;
+
+        private void Awake()
+        {
+            _material = GetComponent<Renderer>().material;
+            _lengthFly = Random.Range(1.0f, 3.0f);
+            _displayBonuses = new DisplayBonuses();
+        }
+
+        protected override void Interaction()
+        {
+            //base.Interaction();
+            _displayBonuses.Display(Point);
+            _goodBonus = FindObjectsOfType<GoodBonus>();
+            //print(_goodBonus.Length);
+            if (_goodBonus.Length <= 1)
+            {
+                print("победа");
+            }
+        }
+
+        public void Fly()
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x,
+                Mathf.PingPong(Time.time, _lengthFly),
+                transform.localPosition.z);
+        }
+
+        public void Flicker()
+        {
+            _material.color = new Color(_material.color.r, _material.color.g, _material.color.b,
+                Mathf.PingPong(Time.time, 1.0f));
+        }
+    }
+}
---
+++ Pshenichnikov/Assets/Scripts/SavedData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RollABall
+{
+    public sealed class SavedData<T> where T : struct
+    {
+        public int CountBonuses;
+        public T IdPlayer = default;
+    }
+}
